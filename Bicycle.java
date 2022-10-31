413b7ccf95d961d8939309b415bedf1b06e998fd
AprilRoseMontiadora <116123112+AprilRoseMontiadora@users.noreply.github.com>
2022-10-31 13:36:30 +0800

    Update Bicycle.java

--- Bicycle.java
+++ Bicycle.java
@@ -1,3 +1,28 @@
-public class Bicycle
-{
+public class Bicycle {
+
+	private String ownerName;
+    private String tagNo;
+	public Bicycle () {
+		ownerName = "Unknown";
+		tagNo = "Unknown";
+   }
+		
+	public Bicycle (String TagNo,String name) {
+		tagNo=TagNo;
+		ownerName =name;	
+
+	}
+	public String getOwnerName (){
+		return ownerName;
+	}
+	public void setOwnerName (String name) {
+		ownerName =name;
+	}
+	public String getTagNo(){
+		return tagNo;
+	}
+	public void setTagNo (String TagNo) {
+		tagNo=TagNo;
+	}
+	
 }
