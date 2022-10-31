14c38cf692af7e32408896091d55ad23e89ee6a3
AprilRoseMontiadora <116123112+AprilRoseMontiadora@users.noreply.github.com>
2022-10-31 13:39:42 +0800

    Update BicycleRegistration.java

--- BicycleRegistration.java
+++ BicycleRegistration.java
@@ -1,3 +1,36 @@
 public class BicycleRegistration
 {
+
+	public static void main(String [] args) {
+		Bicycle bike1, bike2, bike3,moy;
+		String owner1,owner2,tagNo,owner;
+		
+		bike1= new Bicycle ();
+		bike1.setOwnerName("Adams Smith");
+
+		bike2= new Bicycle ();
+		bike2.setOwnerName("Ben Jones");
+		
+		bike3= new Bicycle ();
+		bike3.setTagNo("2004-134R");
+		
+		moy= new Bicycle ();
+		moy.setOwnerName("April");
+		moy.setTagNo("2022");
+		
+		
+
+		owner1= bike1.getOwnerName();
+		owner2= bike2.getOwnerName();
+    tagNo=bike3.getTagNo();
+		owner=moy.getOwnerName();
+		tagNo=moy.getTagNo();
+		
+		System.out.println(owner1 + " owns a bicycle.");
+		System.out.println(owner2 + " also owns a bicycle.");
+		System.out.println("2004-134R");
+		System.out.println(owner+ " " + tagNo);
+	}
+	
+
 }
